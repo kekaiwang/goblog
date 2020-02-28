e20c445582ca067f07a1c2265a3a53737282ea8c
kkwang <wkekai@163.com>
2020-02-28 21:52:59 +0800

    add logs

--- main.go
+++ main.go
@@ -13,10 +13,14 @@ func init() {
 }
 
 func main() {
-	orm.Debug = true
+	orm.Debug = false
 
 	beego.AddFuncMap("sub", sub)
 	beego.AddFuncMap("add", add)
+
+	// logs.Async()
+	// logs.SetLogger(logs.AdapterMultiFile, `{"filename": "logs/test.log"}`)
+	// logs.SetLogger(logs.AdapterConn, `{"net":"tcp","addr":":7020"}`)
 
 	// if runmode is dev use this
 	// if beego.BConfig.RunMode == "dev" {
